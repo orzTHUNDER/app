1e4bfd2de81ff500d09027061d45a0b1aa86f849
orzTHUNDER <a.r.nandhabeast@gmail.com>
2024-03-07 13:29:06 +0530

    1). Added jakarta validation and hibernate validator for validation 2). Added Valid annotation in signup controller and mapped with exception handler for giving all the exception 3). Changed the bean MODEL for having email, password, fullname, role fields to have validations

--- src/main/java/com/hackathon/controller/UserController.java
+++ src/main/java/com/hackathon/controller/UserController.java
@@ -1,27 +1,37 @@
 package com.hackathon.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hackathon.model.User;
 import com.hackathon.service.UserService;
+
+import jakarta.validation.Valid;
+
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @RestController
-@RequestMapping("user")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
     @PostMapping("signup")
-    public ResponseEntity<?> signUp(@RequestBody User user) {
+    public ResponseEntity<?> signUp(@Valid @RequestBody User user) {
         System.out.println(user);
         return userService.signUp(user);
     }
@@ -31,6 +41,18 @@ public class UserController {
         // TODO: process POST request
         return userService.login(user);
 
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<Object> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 
 }
--- src/main/java/com/hackathon/model/User.java
+++ src/main/java/com/hackathon/model/User.java
@@ -6,6 +6,10 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,16 +24,25 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int userId;
 
+    @NotEmpty(message = "Full name must not be empty")
+    @Size(max = 100, message = "Full name must be less than 100 characters")
     private String fullName;
 
+    @NotEmpty(message = "Email must not be empty")
+    @Email(message = "Email should be valid")
     private String email;
 
+    @NotEmpty(message = "Password must not be empty")
+    @Size(min = 8, message = "Password must be at least 8 characters long")
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$", message = "Password must be at least 8 characters long, contain at least one digit, one uppercase letter, one lowercase letter, and one special character")
     private String password;
 
     @ManyToOne
     @JoinColumn(name = "teamId", referencedColumnName = "teamId")
     private Team team;
 
+    @NotEmpty(message = "Role must not be empty")
+    @Pattern(regexp = "^(Participant|Panelist|Judge)$", message = "Role must be either Participant, Panelist, or Judge")
     private String role;
 
     private String experience;
