b9309ff4c06d4a53da9495cd5f945b0b1f53a82c
orzTHUNDER <a.r.nandhabeast@gmail.com>
2024-03-07 14:30:03 +0530

    Added idea and validation in team model

---
+++ src/main/java/com/hackathon/model/Idea.java
@@ -0,0 +1,12 @@
+package com.hackathon.model;
+
+public class Idea {
+
+    private int ideaId;
+    private Team team;
+    private int panelistId;
+    private String ideaTitle;
+    private String ideaDescription;
+    private String status;
+
+}
--- src/main/java/com/hackathon/model/Team.java
+++ src/main/java/com/hackathon/model/Team.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,7 +19,12 @@ public class Team {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int teamId;
+
+    @NotEmpty(message = "Full name must not be empty")
+    @Size(max = 100, message = "Full name must be less than 100 characters")
     private String teamName;
+
+    @NotEmpty(message = "Leader Id cannot be empty")
     private String leaderId;
 
 }
